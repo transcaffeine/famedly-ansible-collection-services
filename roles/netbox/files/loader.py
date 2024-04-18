famedly/ansible-collection-services

feat(netbox): initial role

roles/netbox/templates/loader.py
@@ -1,0 +1,12 @@
+import yaml
+import os
+
+def __pfusch(path):
+  for node in os.scandir(path):
+    if node.is_dir():
+      load_dir(node.path)
+    else:
+      for k,v in yaml.safe_load(open(node.path)).items():
+        globals()[k] = v # we just add *all* the kv pairs to the scope netbox will use to access it's config
+
+__pfusch(os.environ.get('NETBOX_YAML_CONFIG_DIR','/opt/netbox/config'))

transcaffeine/famedly-ansible-collection-services

roles/netbox/files/loader.py
@@ -1,0 +1,12 @@
+import yaml
+import os
+
+def load_dir_rec(path):
+  for node in os.scandir(path):
+    if node.is_dir():
+      load_dir(node.path)
+    else:
+      for k,v in yaml.safe_load(open(node.path)).items():
+        globals()[k] = v # we just add *all* the kv pairs to the scope netbox will use to access it's config
+
+load_dir_rec(os.environ.get('NETBOX_YAML_CONFIG_DIR','/opt/netbox/config'))